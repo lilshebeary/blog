c837ae74ccd991cf7801f4bfca7a2e362ccd8b4f
Ciri Ryan <cirijaye@yahoo.com>
2021-09-16 11:35:11 -0700

    add useContext to IndexScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,9 +4,10 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({children}) => {
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={"hello"}>
             {children}
         </BlogContext.Provider>
     );
 };
 
+export default BlogContext;
