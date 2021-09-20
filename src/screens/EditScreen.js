8f010e5322feefe074e3a812c9a5c723e6a705e5
Ciri Ryan <cirijaye@yahoo.com>
2021-09-20 06:08:28 -0700

    add icon and edit navigation

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,58 @@
+import React, { useContext, useState }from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { Context } from '../context/BlogContext'
+
+const EditScreen = ({ navigation }) => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState();
+    const { addBlogPost } = useContext(Context);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput 
+                value={title}
+                onChangeText={text => setTitle(text)}
+                style={styles.input}
+            />
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput
+                value={content}
+                onChangeText={text => setContent(text)}
+                style={styles.input}
+            />
+            <Button 
+                title="Add Blog Post" 
+                onPress={() => {
+                    addBlogPost(title, content, () => {
+                        navigation.navigate('Index');
+                    });
+                   
+                }}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 25
+    },
+    input: {
+        fontSize: 20, 
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 35,
+        padding: 5,
+        margin: 15
+    },
+    label: {
+        fontSize: 25,
+        marginHorizontal: 15,
+
+
+    }
+
+});
+
+export default EditScreen;
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,29 +1,42 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
-   const { state } = useContext(Context);
+	const { state } = useContext(Context);
 
-   const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+	const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{blogPost.title}</Text>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>{blogPost.title}</Text>
+			<Text style={styles.content}>{blogPost.content}</Text>
+		</View>
+	);
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+				<EvilIcons name="pencil" size={35} />
+			</TouchableOpacity>
+		)
+	};
 };
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        marginTop: 25
-    },
-    title: {
-       
-        fontSize: 24,
-        
-    }
+	container: {
+		alignItems: 'center',
+		marginTop: 25
+	},
+	title: {
+		fontSize: 24
+	},
+	content: {
+		fontSize: 22
+	}
 });
 
 export default ShowScreen;
